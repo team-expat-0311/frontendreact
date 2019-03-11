0d47fd540fbc39f83e7f78586fec5439d372d779
Francis Chen <fncis.chen@gmail.com>
2019-03-11 16:00:40 -0700

    photos component

--- frontendfrancis/src/Components/Photos.js
+++ frontendfrancis/src/Components/Photos.js
@@ -4,22 +4,31 @@ import axios from 'axios';
 import Photo from "./Photo";
 
 class Photos extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
 
-        this.state = {
-            photos: []
-        }
+        this.state={
+            photoList:[]
+        };
     }
 
     componentDidMount() {
+        axios.get("https://expat-journal.herokuapp.com/api/photos/all")
+            .then(function(res){
 
+                // let photoList = res.data;
+                this.setState({photoList: res.data});
+                // console.log(this.state.photos)
+                })
+            .catch(err => console.log(err));
+
+            console.log(this.state.photoList)
     }
 
     render() {
     return (
         <div className="photos">
-            {this.state.photos.map(a_photo =>
+            {this.state.photoList.map(a_photo =>
                 
             <Photo photo={a_photo} />    
 
