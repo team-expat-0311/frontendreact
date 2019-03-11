79be1b89b4fca5230f8f17ee3edc4c376b43b01a
Francis Chen <fncis.chen@gmail.com>
2019-03-11 15:38:38 -0700

    got login and register components connected to api and working

--- frontendfrancis/src/Components/LogIn.js
+++ frontendfrancis/src/Components/LogIn.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-const LogIn = props => {
-    return (
-        <div className="login">
-        </div>
-    )
+class LogIn extends Component {
+
+    constructor(){
+        super()
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+    }
+
+    handleChanges = e => {
+        e.preventDefault();
+        console.log(e.target.name, e.target.value )
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    loggingin = e => {
+        e.preventDefault();
+        let data = {username: this.state.username, password: this.state.password}
+        console.log(data);
+        axios.post("https://expat-journal.herokuapp.com/api/auth/login", data)
+            .then(function(res){
+                console.log(res);
+                localStorage.setItem("token", res.data.token);
+            })
+            .catch(err => console.log(err));
+        console.log("Done!")
+        this.props.history.push('/photos');
+    }
+
+    render() {
+
+        return (
+            <div className="login">
+                <form onSubmit={this.loggingin}>
+                    <input type="text" name="username" onChange={this.handleChanges}></input>
+                    <input type="text" name="password" onChange={this.handleChanges}></input>
+
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
+        )
+
+    }
 }
 
 export default LogIn;
--- frontendfrancis/src/Components/Photos.js
+++ frontendfrancis/src/Components/Photos.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const Photos = props => {
+import Photo from "./Photo";
+
+class Photos extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            photos: []
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
     return (
         <div className="photos">
+            {this.state.photos.map(a_photo =>
+                
+            <Photo photo={a_photo} />    
+
+            )}
         </div>
     )
+    }
 }
 
 export default Photos
