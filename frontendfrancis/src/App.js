62dd5a6a2c16ffb004303daf58baaa18320e0566
Francis Chen <fncis.chen@gmail.com>
2019-03-13 13:43:36 -0700

    made changes to css

--- frontendfrancis/src/App.js
+++ frontendfrancis/src/App.js
@@ -12,30 +12,26 @@ import ProtectedRoute from "./Routers/ProtectedRoute";
 
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import './photos.css';
 import "./register.css";
-=======
-import "./Project.css";
->>>>>>> 93a470b77cf2a575e8b2a7234cf98e03022147d5
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <nav>
-          <Link to="/" className="link">Home</Link>
-          <Link to="/register" className="link">Register</Link>
-          <Link to="/login" className="link">Login</Link>
-          <Link to="/photos" className="link">Public Photos</Link>
-          <Link to="/private" className="link"> Private</Link>
+          <Link to="/">Home</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/photos">Public Photos</Link>
+          <Link to="/private">Private</Link>
         </nav>
 
         <Route exact path="/" component={Home}/>
         <Route path="/register" component={Register}/>
         <Route path="/login" component={LogIn}/>
         <Route path="/photos" component={Photos}/>
-        <Route path="/photos/:id"  component={<Photo/>}/>
+
         <ProtectedRoute path="/private" component={PhotosPrivate}/>
       </div>
     );
